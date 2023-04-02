838a061a3cd497c50435fcfbb0d8cabb6b2910a8
Shrinivas Nawgire <shrinivasnawgire@gmail.com>
2023-04-02 11:49:42 +0530

    Created a column series chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts/highstock';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+  title = "Highcharts demo";
+  highcharts = Highcharts;
+  data: any;
+
+  constructor(private http: HttpClient) {
+    this.http.get('./assets/data.json').subscribe(data => {
+      this.data = data;
+      this.highcharts.stockChart('container', {
+        chart: {
+          alignTicks: false
+        },
+        rangeSelector: {
+          selected: 1,
+          allButtonsEnabled: true,
+        },
+        plotOptions: {
+          column: {
+            pointWidth: 20
+          }
+        },
+        navigator: {
+          enabled: false
+        },
+        scrollbar: {
+          enabled: false
+        },
+        title: {
+          text: 'AAPL Stock Volume'
+        },
+        yAxis: {
+          opposite: false
+        },
+        series: [{
+          type: 'column',
+          name: 'AAPL Stock Volume',
+          data: this.data ? this.data : [],
+          dataGrouping: {
+            units: [[
+              'week',
+              [1]
+            ], [
+              'month',
+              [1, 2, 3, 4, 6]
+            ]]
+          }
+        }]
+      });
+    })
+  }
+
 }
